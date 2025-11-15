f079ceea4b47f1474f1d63b81172ec431083b76f
Brandon Cullen <94326161+BCullen2201@users.noreply.github.com>
2025-11-15 12:46:43 -0500

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "randNumGen.c"
+
+#define BUF 50
+
+struct answerString {
+    char string[BUF];
+};
+
+int sleep_ms(int ms) {
+    usleep(ms * 1000);
+}
+
+const char * getAnswer() {
+    int selection = getRandomNumber(0,19);
+    const int length = getRandomNumber(45,75);
+    int sleepTime = 10;
+    char * answer;
+    char answerArray[20][BUF] = {
+        {"Yep, definitely"},
+        {"Not sure, try again"},
+        {"Don't rely on it"},
+        {"Seems the answer is yes"},
+        {"No doubt!"},
+        {"Come back later"},
+        {"I'm gonna say no"},
+        {"I shouldn't say right now"},
+        {"They're telling me no"},
+        {"Absolutely, yes"},
+        {"You can count on it"},
+        {"The way it looks, yes"},
+        {"Hard to judge currently"},
+        {"It ain't looking good for you"},
+        {"It's to be expected"},
+        {"Focus and have another go"},
+        {"I really doubt it"},
+        {"It's looking good!"},
+        {"Affirmative!"},
+        {"They're saying yes"}
+    };
+
+    if (selection == 19) {
+        selection--;
+    }
+    for (int i = 0; i < length; i++) {
+        selection++;
+        if (selection > 19) {
+            selection = 0;
+        }
+
+        if (i != length - 1) {
+            printf("%s\n", answerArray[selection]);
+            sleep_ms(sleepTime);
+            sleepTime = sleepTime + 5;
+        }
+
+        system("clear");
+    }
+
+    answer = answerArray[selection];
+    return answer;
+}
+
+int main() {
+    struct answerString s1;
+    strcpy(s1.string, getAnswer());
+
+    printf("%s\n\n", s1.string);
+
+    return 0;
+}
---
+++ src/randNumGen.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <fcntl.h>
+
+int getRandomNumber(int MIN, int MAX) {
+	if (MIN > MAX) {
+		int holder = MIN;
+		MIN = MAX;
+		MAX = holder;
+	}
+
+	const int BUFFER = 20;
+	int randomData = open("/dev/urandom", O_RDONLY);
+	if (randomData == -1) {
+		fprintf(stderr, "Could not open '/dev/urandom'\n");
+		exit(1);
+	}
+	unsigned int result[BUFFER];
+
+	read(randomData, result, BUFFER);
+	close(randomData);
+
+	int number = (*result % (MAX - MIN + 1)) + MIN;
+
+	return number;
+}
