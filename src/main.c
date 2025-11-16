fd9bc10d5c01dbddb9ea5867eccbb22110a27222
Brandon Cullen <bjacullen01@proton.me>
2025-11-16 11:19:03 -0500

    Created header file for randNumGen, included missing standard header files in main.c and randNumGen.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#include "randNumGen.c"
+#include "randNumGen.h"
 
 #define BUF 50
 
---
+++ src/randNumGen.h
@@ -0,0 +1,8 @@
+#ifndef randNumGen
+
+#define randNumGen
+
+int getRandomNumber(int MIN, int MAX);
+
+#endif
+
