b3291b82991a98c808cc347e54239a4be726a211
BCullen2201 <bjacullen01@proton.me>
2025-11-19 14:40:51 -0500

    Had a mistake in my Makefile, header file for randNumGen not needed

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "randNumGen.h"
+#include "randNumGen.c"
 
 #define BUF 30
 
--- src/randNumGen.h
+++
@@ -1,8 +0,0 @@
-#ifndef randNumGen
-
-#define randNumGen
-
-int getRandomNumber(int MIN, int MAX);
-
-#endif
-
